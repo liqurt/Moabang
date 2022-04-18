a4baaa88004d548685947a05cf6ca11d47f81ae0
qnfzks111 <qnfzks111@naver.com>
2022-04-18 10:23:54 +0900

    [#S06P31D205-19] chore : SpringBoot 프로젝트 초기화

---
+++ BE/Readme.md
@@ -0,0 +1,7 @@
+|             | version        |
+| ----------- | -------------- |
+| JDK         | 11             |
+| Project     | Gradle Project |
+| Language    | java           |
+| Spring Boot | 2.6.6          |
+
---
+++ BE/src/test/java/com/self/roomescape/RoomescapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.roomescape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomescapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
