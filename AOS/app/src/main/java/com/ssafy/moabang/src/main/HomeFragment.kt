383b24b87c56251bde732e4b239affcceffa31c6
김현수 <hskim9706@gmail.com>
2022-04-19 10:51:16 +0900

    [S06P31D205-18] feat : :sparkles: 필요한 fragment들 생성

---
+++ AOS/app/src/main/java/com/ssafy/moabang/src/main/HomeFragment.kt
@@ -0,0 +1,25 @@
+package com.ssafy.moabang.src.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ssafy.moabang.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+}
