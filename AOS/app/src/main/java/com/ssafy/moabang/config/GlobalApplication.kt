b5e22191fa725bcd3c7e0cd66ca3ff0f5efaa553
김현수 <hskim9706@gmail.com>
2022-04-19 14:40:40 +0900

    [S06P31D205-6] chore : :apple: 네이버 로그인 세팅

--- AOS/app/src/main/java/com/ssafy/moabang/config/GlobalApplication.kt
+++ AOS/app/src/main/java/com/ssafy/moabang/config/GlobalApplication.kt
@@ -1,9 +1,12 @@
 package com.ssafy.moabang.config
 
 import android.app.Application
+import android.os.Build
 import com.kakao.sdk.common.KakaoSdk
+import com.nhn.android.naverlogin.OAuthLogin
 import com.ssafy.moabang.BuildConfig
 import com.ssafy.moabang.R
+import com.ssafy.moabang.src.login.LoginActivity
 
 class GlobalApplication : Application() {
     override fun onCreate() {
@@ -11,6 +14,9 @@ class GlobalApplication : Application() {
         instance = this
 
         KakaoSdk.init(this, BuildConfig.native_app_key)
+
+        val mOAuthLoginModule = OAuthLogin.getInstance()
+        mOAuthLoginModule.init(this, BuildConfig.naver_client_id, BuildConfig.naver_client_secret, "모아방")
     }
 
     companion object {
