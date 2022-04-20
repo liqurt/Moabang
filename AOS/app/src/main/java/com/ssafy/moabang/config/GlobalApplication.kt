ead3ee9107400d8ed93a420127d754e87badb396
윤승일 <liqurtjob@gmail.com>
2022-04-20 18:06:27 +0900

    [S06P31D205-7] feat : :sparkles: 카카오 로그인 - Token 백엔드에 전달하기!

--- AOS/app/src/main/java/com/ssafy/moabang/config/GlobalApplication.kt
+++ AOS/app/src/main/java/com/ssafy/moabang/config/GlobalApplication.kt
@@ -1,20 +1,41 @@
 package com.ssafy.moabang.config
 
 import android.app.Application
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import com.kakao.sdk.common.KakaoSdk
 import com.ssafy.moabang.BuildConfig
-import com.ssafy.moabang.R
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class GlobalApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
 
-        KakaoSdk.init(this, BuildConfig.native_app_key)
+        KakaoSdk.init(this, BuildConfig.native_app_key) // 카카오 세팅
+
+        val serverURL="http://114.129.238.28" // 승관의 집
+
+        gson = GsonBuilder()
+            .setLenient()
+            .create()
+
+        retrofit = Retrofit.Builder()
+            .baseUrl(serverURL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
     }
 
     companion object {
         lateinit var instance: GlobalApplication
+            private set
+
+        lateinit var retrofit: Retrofit
+            private set
+
+        lateinit var gson: Gson
             private set
     }
 }
