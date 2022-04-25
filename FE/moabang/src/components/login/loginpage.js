9a93e58525e1e694abb3504a6ff1284e21590aa8
최성석 <tjdtjr234@naver.com>
2022-04-25 10:21:09 +0900

    [S06P31D205-8]feat : :sparkles: 로그아웃 버튼 추가

--- FE/moabang/src/components/login/loginpage.js
+++ FE/moabang/src/components/login/loginpage.js
@@ -1,23 +1,33 @@
+/**
+* react-google-login을 이용한 구글로그인 구현
+* react-google-login의 로그아웃 기능 사용 시 
+* 재로그인을 할 때 자동 로그인이 되기에 로그아웃은 따로 코드 구현
+* @author 최성석
+* @version 1.0
+* 
+*/
 import React from 'react';
 import './loginpage.css'
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { GoogleLogin } from 'react-google-login';
 import axios from "axios";
 import logo from '../login/모아방 로고.png';
 
+//보안 상의 이유로 .env에 저장된 구글 클라이언트 아이디 값 불러오기
 const Google_clinet_id = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 const loginpage = () => {
-
+    //로그인 성공 시
     const onSuccessGoogle = async (response) => {
         console.log(response);
-        const { googleId, profileObj: { email, name }, accessToken } = response;
+        const { accessToken } = response;
         const data = {
             accessToken: accessToken
         }
         console.log(data);
 
-        // 로그인 성공한 경우 access토큰을 백으로 보내 
+        // 로그인 성공한 경우 access토큰을 백으로 보내 줌
         await axios.get('/test/login',
             {
+                //헤더에 accessToken값을 담아 보냄
                 headers: {
                     code: `${accessToken}`
                 }
@@ -36,11 +46,11 @@ const loginpage = () => {
         console.log(error);
     }
 
-    //로그아웃
+    //로그아웃 성공 시
     const onLogoutSuccess = () => {
         alert("로그아웃 되었습니다!");
     };
-
+    //로그아웃
     const onLogout = () => {
         if (window.gapi) {
             const auth2 = window.gapi.auth2.getAuthInstance();
