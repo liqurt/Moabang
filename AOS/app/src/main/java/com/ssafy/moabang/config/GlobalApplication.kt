c797e7c92c13ff8dac826784d753a47fa3771f2f
김현수 <hskim9706@gmail.com>
2022-04-25 15:43:15 +0900

    [S06P31D205-6] feat : :sparkles: GlobalApplication 수정

--- AOS/app/src/main/java/com/ssafy/moabang/config/GlobalApplication.kt
+++ AOS/app/src/main/java/com/ssafy/moabang/config/GlobalApplication.kt
@@ -1,34 +1,24 @@
 package com.ssafy.moabang.config
 
 import android.app.Application
-import android.os.Build
 import com.kakao.sdk.common.KakaoSdk
-import com.nhn.android.naverlogin.OAuthLogin
 import com.ssafy.moabang.BuildConfig
-import com.ssafy.moabang.R
-import com.ssafy.moabang.src.login.LoginActivity
-
+import com.ssafy.moabang.util.SharedPreferencesUtil
 class GlobalApplication : Application() {
     private var instance: GlobalApplication? = null
-    private var mGlobalUserLoginInfo: List<String?> = ArrayList()
 
-    fun getGlobalApplicationContext(): GlobalApplication? {
-        checkNotNull(instance) { "This Application does not GlobalAuthHelper" }
-        return instance
-    }
-
-    fun getGlobalUserLoginInfo(): List<String?>? {
-        return mGlobalUserLoginInfo
-    }
-
-    fun setGlobalUserLoginInfo(userLoginInfo: List<String?>) {
-        mGlobalUserLoginInfo = userLoginInfo
+    companion object{
+        const val SHARED_PREFERENCES_NAME = "MOABANG"
+        lateinit var sSharedPreferences: SharedPreferencesUtil
+        const val COOKIES_KEY_NAME = "cookies"
+        const val JWT = "JWT"
     }
 
     override fun onCreate() {
         super.onCreate()
         instance = this
 
+        sSharedPreferences = SharedPreferencesUtil(applicationContext)
         KakaoSdk.init(this, BuildConfig.native_app_key)
     }
 
