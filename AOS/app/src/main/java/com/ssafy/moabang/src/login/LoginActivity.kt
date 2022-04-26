376fcb5561cb94f74fa449234f37f7ecb1ec871c
윤승일 <liqurtJob@gmail.com>
2022-04-26 13:45:25 +0900

    [S06P31D205-52] feat : :sparkles: 카카오 로그인 심화 - 로그인 성공 시, 자체 토큰 받아서 SharedPrefs에 저장 - 로그아웃 - 카카오 로그인 정보로 마이페이지 UI 변경 - 디스플레이 라이트 모드일때, 타이틀 바 안 보이도록 수정

--- AOS/app/src/main/java/com/ssafy/moabang/src/login/LoginActivity.kt
+++ AOS/app/src/main/java/com/ssafy/moabang/src/login/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.ssafy.moabang.src.login
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -15,6 +16,7 @@ import com.ssafy.moabang.src.main.MainActivity
 import androidx.activity.viewModels
 import com.ssafy.moabang.config.GlobalApplication
 import com.ssafy.moabang.src.retrofitInterface.loginService
+import okhttp3.Headers
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -42,9 +44,26 @@ class LoginActivity : AppCompatActivity() {
         binding.tvLoginATmp.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
+
+        doIhaveKakaoToken()
     }
 
-    fun kakaoLogin(view : View) {
+    fun doIhaveKakaoToken(){
+        UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
+            if (error != null) {
+                Log.e("AAAAA", "토큰 정보 보기 실패", error)
+            }
+            else if (tokenInfo != null) {
+                Log.i("AAAAA", "토큰 정보 보기 성공" +
+                        "\n회원번호: ${tokenInfo.id}" +
+                        "\n만료시간: ${tokenInfo.expiresIn} 초")
+                Log.i("AAAAA", "알잘딱 로그인")
+                kakaoLogin(null)
+            }
+        }
+    }
+
+    fun kakaoLogin(view : View?) {
         val context = this
         // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
         if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
@@ -73,18 +92,17 @@ class LoginActivity : AppCompatActivity() {
      * 카카오톡 로그인 성공 후 토큰을 서버에 전송한다.
      */
     fun sendTokenToBackend(token: OAuthToken) {
-        Log.d("AAAAA","kakaoToken : ${token.accessToken}")
         val loginService = GlobalApplication.retrofit.create(loginService::class.java)
         loginService.login(token.accessToken).enqueue(object : Callback<Boolean>{
             override fun onResponse(call: Call<Boolean>, response: Response<Boolean>) {
                 if(response.isSuccessful){
-                    Log.d("AAAAA","${response.body()}") // true == 로그인 성공
+                    val moabangToken = response.headers()
+                    saveOurTokenToSharedPrefs(moabangToken)
                     val intent = Intent(this@LoginActivity, MainActivity::class.java)
                     startActivity(intent)
                 }else{
                     Log.e("AAAAA","네트워킹 성공, 하지만 원하는 결과가 아님. ${response.errorBody()}")
-                    Toast.makeText(this@LoginActivity, "네트워크 성공,  : ${response.errorBody()}", Toast.LENGTH_SHORT).show()
-
+                    Toast.makeText(this@LoginActivity, "네트워크 성공했지만 에러발생 : ${response.errorBody()}", Toast.LENGTH_SHORT).show()
                 }
             }
 
@@ -93,5 +111,12 @@ class LoginActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+    fun saveOurTokenToSharedPrefs(moabangToken: Headers) {
+        val sp = getSharedPreferences("moabang", MODE_PRIVATE)
+        val editor  : SharedPreferences.Editor = sp.edit()
+        editor.putString("moabangToken",moabangToken["Authorization"])
+        editor.apply()
     }
 }
--- AOS/app/src/main/java/com/ssafy/moabang/src/main/MyPageFragment.kt
+++ AOS/app/src/main/java/com/ssafy/moabang/src/main/MyPageFragment.kt
@@ -1,25 +1,71 @@
 package com.ssafy.moabang.src.main
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.kakao.sdk.user.UserApiClient
 import com.ssafy.moabang.databinding.FragmentMyPageBinding
+import com.ssafy.moabang.src.login.LoginActivity
 
 class MyPageFragment : Fragment() {
     private lateinit var binding: FragmentMyPageBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentMyPageBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvMypageFLogout.setOnClickListener { logout() }
+        setUIwithKakao()
+    }
+
+    /**
+     * 로그아웃 후, Login Activity로 간다.
+     */
+    private fun logout(){
+        UserApiClient.instance.unlink { error ->
+            if (error != null) {
+                Log.e("AAAAA", "연결 끊기 실패", error)
+            } else {
+                Log.i("AAAAA", "연결 끊기 성공. SDK에서 토큰 삭제 됨")
+            }
+        }
+        val intent = Intent(requireActivity(), LoginActivity::class.java)
+        startActivity(intent)
+    }
+
+    /**
+     * 카카오에 있는 정보를 바탕으로 UI 변경
+     */
+    private fun setUIwithKakao(){
+        UserApiClient.instance.me { user, error ->
+            if (error != null) {
+                Log.e("AAAAA", "사용자 정보 요청 실패", error)
+            }
+            else if (user != null) {
+                Log.i("AAAAA", "사용자 정보 요청 성공" +
+                        "\n회원번호: ${user.id}" +
+                        "\n이메일: ${user.kakaoAccount?.email}" +
+                        "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
+                        "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}")
+
+                binding.tvMypageFUserName.text = user.kakaoAccount?.profile?.nickname
+                binding.tvMypageFUserEmail.text = user.kakaoAccount?.email
+                Glide.with(this)
+                    .load(user.kakaoAccount?.profile?.thumbnailImageUrl)
+                    .into(binding.civMypageF)
+            }
+        }
     }
 
 }
