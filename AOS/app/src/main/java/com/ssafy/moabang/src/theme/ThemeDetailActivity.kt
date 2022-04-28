9602cf03c19e82df61346b22dcc051f6421e6f7a
김현수 <hskim9706@gmail.com>
2022-04-28 10:50:37 +0900

    [S06P31D205-33]style : :art: themeDetailActivity 추가

---
+++ AOS/app/src/main/java/com/ssafy/moabang/src/theme/ThemeDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.ssafy.moabang.src.theme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ssafy.moabang.R
+
+class ThemeDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_theme_detail)
+    }
+}
