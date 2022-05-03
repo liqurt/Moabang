1f0343a954587641dbe6dd1d8e8c71eaa42f6310
qnfzks111 <qnfzks111@naver.com>
2022-05-03 16:21:01 +0900

    fix : ApiOperation 수정

--- BE/src/main/java/com/self/roomescape/controller/CafeController.java
+++ BE/src/main/java/com/self/roomescape/controller/CafeController.java
@@ -5,6 +5,7 @@ import com.self.roomescape.entity.Theme;
 import com.self.roomescape.repository.CafeRepository;
 import com.self.roomescape.repository.ThemeRepository;
 import com.self.roomescape.repository.mapping.ThemeListMapping;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +21,7 @@ public class CafeController {
     @Autowired
     private ThemeRepository themeRepository;
 
+    @ApiOperation(value = "카페 전체 목록", notes = "카페 전체 목록을 반환.")
     @GetMapping("/list")
     public ResponseEntity<?> findList() {
         List<Cafe> cafeList = cafeRepository.findAll();
@@ -28,6 +30,7 @@ public class CafeController {
         return new ResponseEntity<>(cafeList, HttpStatus.OK);
     }
 
+    @ApiOperation(value = "테마 1개 검색", notes = "해당 tid에 해당하는 테마 정보를 반환")
     @GetMapping("/theme/{cid}")
     public ResponseEntity<?> findCafeTheme(@PathVariable("cid") int cid) {
         List<Theme> themeList = themeRepository.findByCid(cid);
@@ -35,6 +38,7 @@ public class CafeController {
         return new ResponseEntity<>(themeList, HttpStatus.OK);
     }
 
+    @ApiOperation(value = "테마 전체 목록", notes = "테마 전체 목록을 카페 이름 및 URL과 같이 반환")
     @GetMapping("/theme/list")
     public ResponseEntity<?> findAllTheme() {
         List<ThemeListMapping> themeAllList = themeRepository.findThemeAndCafe();
@@ -42,6 +46,7 @@ public class CafeController {
         return new ResponseEntity<>(themeAllList, HttpStatus.OK);
     }
 
+    @ApiOperation(value = "테마 상세 정보", notes = "해당 Tid를 통해서 테마 상세 정보 및 댓글 목록을 반환한다.")
     @GetMapping("/theme/listtest")
     public ResponseEntity<?> find2AllTheme() {
         List<ThemeListMapping> themeAllList = themeRepository.findThemeAndCafe();
