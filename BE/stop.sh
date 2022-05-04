ad875cce7826bc2c93c952bcc88350dd8a315bba
정경훈 <jungkh94@naver.com>
2022-05-04 05:24:27 +0000

    fix : :bug: 버그 수정

--- BE/stop.sh
+++ BE/stop.sh
@@ -3,6 +3,9 @@
 CURRENT_PID=$(ps -ef | grep java | grep moabang)
 if [ -z ${CURRENT_PID} ] ;then
 echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+export JENKINS_NODE_COOKIE=dontKillMe
+export BUILD_ID=dontKillMe
+nohup java -jar /var/lib/jenkins/workspace/test/BE/build/libs/moabang-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
 else
 echo "> kill -9 $CURRENT_PID"
 kill -9 $CURRENT_PID
