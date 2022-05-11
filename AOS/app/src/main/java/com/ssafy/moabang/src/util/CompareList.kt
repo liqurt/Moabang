5f1b1714638d12fd7bbdc61226d33c38344f6475
김현수 <hskim9706@gmail.com>
2022-05-11 14:09:58 +0900

    [S06P31D205-88]feat : :sparkles: 테마 비교용 dto, object 생성

---
+++ AOS/app/src/main/java/com/ssafy/moabang/src/util/CompareList.kt
@@ -0,0 +1,7 @@
+package com.ssafy.moabang.src.util
+
+import com.ssafy.moabang.data.model.dto.ThemeForCompare
+
+object CompareList {
+    var items : MutableList<ThemeForCompare> = mutableListOf()
+}
