1846b5237e1dce00f63933770e0c43f7c37e6a12
김현수 <hskim9706@gmail.com>
2022-05-11 15:36:30 +0900

    [S06P31D205-88]style : :art: 바텀시트 RVA 구현 및 연결

---
+++ AOS/app/src/main/java/com/ssafy/moabang/adapter/CompareTitleListRVAdapter.kt
@@ -0,0 +1,45 @@
+package com.ssafy.moabang.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.ssafy.moabang.R
+import com.ssafy.moabang.config.GlobalApplication
+import com.ssafy.moabang.data.model.dto.Theme
+import com.ssafy.moabang.data.model.dto.ThemeForCompare
+import com.ssafy.moabang.data.model.repository.Repository
+import com.ssafy.moabang.data.model.viewmodel.ThemeViewModel
+import com.ssafy.moabang.databinding.ListThemeItemBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class CompareTitleListRVAdapter: RecyclerView.Adapter<CompareTitleListRVAdapter.ViewHolder>() {
+    var data: MutableList<ThemeForCompare> = mutableListOf()
+    lateinit var binding: ListThemeItemBinding
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        fun bind(item: ThemeForCompare){
+            itemView.findViewById<TextView>(R.id.tv_ctItem_title).text = item.tname
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder{
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_compare_title_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+
+
+    override fun getItemCount(): Int = data.size
+}
--- AOS/app/src/main/java/com/ssafy/moabang/src/theme/ThemeCompareActivity.kt
+++ AOS/app/src/main/java/com/ssafy/moabang/src/theme/ThemeCompareActivity.kt
@@ -2,11 +2,16 @@ package com.ssafy.moabang.src.theme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ssafy.moabang.R
+import com.ssafy.moabang.adapter.CompareTitleListRVAdapter
 import com.ssafy.moabang.databinding.ActivityThemeCompareBinding
+import com.ssafy.moabang.src.util.CompareList
 
 class ThemeCompareActivity : AppCompatActivity() {
     private lateinit var binding: ActivityThemeCompareBinding
+    private lateinit var compareTitleListRVAdapter: CompareTitleListRVAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +24,20 @@ class ThemeCompareActivity : AppCompatActivity() {
     private fun init(){
         binding.toolbarThemeCompare.ivToolbarIcon.setOnClickListener { finish() }
         binding.toolbarThemeCompare.tvToolbarTitle.text = "테마 비교하기"
+
+        if(CompareList.items.isNotEmpty()) binding.tvTcbsBlank.visibility = View.GONE
+
+        setRVAdapter()
+    }
+
+    private fun setRVAdapter(){
+        compareTitleListRVAdapter = CompareTitleListRVAdapter()
+        compareTitleListRVAdapter.data = CompareList.items
+
+        binding.rvTcbs.apply {
+            layoutManager = LinearLayoutManager(this@ThemeCompareActivity, LinearLayoutManager.VERTICAL, false)
+            adapter = compareTitleListRVAdapter
+        }
+
     }
 }
