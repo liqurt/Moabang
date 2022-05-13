78c31b918b2cc31f21ad28c3e945f6d08df63357
윤승일 <liqurtJob@gmail.com>
2022-05-13 18:04:40 +0900

    [S06P31D205-107] feat : :sparkles: 글 작성 화면만 생성

--- AOS/app/src/main/java/com/ssafy/moabang/src/main/community/CommunityDetailActivity.kt
+++ AOS/app/src/main/java/com/ssafy/moabang/src/main/community/CommunityDetailActivity.kt
@@ -1,11 +1,19 @@
 package com.ssafy.moabang.src.main.community
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.ssafy.moabang.R
+import com.ssafy.moabang.config.GlobalApplication.Companion.sp
 import com.ssafy.moabang.data.model.dto.Community
 import com.ssafy.moabang.databinding.ActivityCommunityDetailBinding
+
+private val TAG = "CommunityDetailActivity"
 
 class CommunityDetailActivity : AppCompatActivity() {
 
@@ -30,5 +38,25 @@ class CommunityDetailActivity : AppCompatActivity() {
         binding.tvCommuItemFHeader.text = community.header
         binding.tvCommuItemFTitle.text = community.title
         Glide.with(this).load(community.user.pimg).into(binding.civCommuItemF)
+
+        if (isMine()){
+            binding.UDButtons.visibility = View.VISIBLE
+            binding.btCommuItemFRemove.setOnClickListener { removeCommunity() }
+            binding.btCommuItemFEdit.setOnClickListener { editCommunity() }
+        }else{
+            binding.UDButtons.visibility = View.GONE
+        }
+    }
+
+    private fun editCommunity() {
+        Toast.makeText(this, "편집 미구현", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun removeCommunity() {
+        Toast.makeText(this, "제거 미구현", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun isMine() : Boolean{
+        return community.user.uid == sp.getInt("uid")
     }
 }
--- AOS/app/src/main/java/com/ssafy/moabang/src/main/community/CommunityFragment.kt
+++ AOS/app/src/main/java/com/ssafy/moabang/src/main/community/CommunityFragment.kt
@@ -2,16 +2,12 @@ package com.ssafy.moabang.src.main.community
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.ssafy.moabang.R
 import com.ssafy.moabang.adapter.CommunityRVAdapter
-import com.ssafy.moabang.adapter.Latest3CommunityRVAdapter
 import com.ssafy.moabang.data.model.dto.Community
 import com.ssafy.moabang.data.model.repository.CommunityRepository
 import com.ssafy.moabang.databinding.FragmentCommunityBinding
@@ -24,8 +20,6 @@ import kotlinx.coroutines.*
  * 4. 뷰에 적용
  * 5. clickListener를 단다.
  */
-
-private val LOGTAG = "CommunityFragment"
 
 class CommunityFragment : Fragment() {
     private var currentHeader: String = "전체"
@@ -50,11 +44,11 @@ class CommunityFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setChips()
+        setButtons()
         setAllCommunity()
     }
 
-    private fun setChips() {
+    private fun setButtons() {
         binding.chipAll.setOnClickListener {
             currentHeader = "전체"
             setCurrentCommunity()
@@ -74,6 +68,10 @@ class CommunityFragment : Fragment() {
             currentHeader = "구인"
             setCurrentCommunity()
             binding.rvCommuF3Announcement.visibility = View.VISIBLE
+        }
+        binding.fabWrite.setOnClickListener {
+            val intent = Intent(context, CommunityWriteActivity::class.java)
+            startActivity(intent)
         }
     }
 
