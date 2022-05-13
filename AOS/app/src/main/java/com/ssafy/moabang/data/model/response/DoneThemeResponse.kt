3ac2182328bef814384409339dc45b0416fe1081
김현수 <hskim9706@gmail.com>
2022-05-13 14:32:31 +0900

    [S06P31D205-82]feat : :sparkles: 이용한 테마 리턴값 수정

--- AOS/app/src/main/java/com/ssafy/moabang/data/model/response/DoneThemeResponse.kt
+++ AOS/app/src/main/java/com/ssafy/moabang/data/model/response/DoneThemeResponse.kt
@@ -6,25 +6,12 @@ import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 data class DoneThemeResponse(
-    val time: String,
-    val type: String,
-    val description: String?,
     val tid: Int,
-    val difficulty: Int,
     val tname: String,
-    val rplayer: String,
-    val genre: String,
-    val grade: Double,
-    val activity: String,
-    val cid: Int,
-    var cname: String,
-    val img: String,
-    var island: String,
-    var si: String,
-    var url: String,
-    var islike: Boolean,
-    val min_player: Int,
-    val max_player: Int,
-    val count : Int,
-    val playDate: String
+    val rating: Double,     // from review
+    val isSuccess: Int,     // from theme
+    val player: Int,        // from review
+    var cname: String,      // from theme
+    val img: String,        // from theme
+    val playDate: String    // from review
 )
